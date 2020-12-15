c697317bb69dd732bf94398f87d99f335fc60d7c
whario <mariocepedaortega@gmail.com>
2020-12-15 20:10:48 +0000

    cambio en develop

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,4 +1,4 @@
 import React from "react";
 import "../../styles/home.scss";
 
-export const Home = () => <div className="text-center mt-5">Hola</div>;
+export const Home = () => <div className="text-center mt-5">Hola desde develop</div>;
