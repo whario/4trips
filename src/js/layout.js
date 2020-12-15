28188aea69ed710bec63c8adbc55ebda3999d4cb
whario <mariocepedaortega@gmail.com>
2020-12-15 19:58:58 +0000

    limpiando archivo

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,14 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop";
-
-import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
 import injectContext from "./store/appContext";
 
-import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
+import { Home } from "./views/home";
 
 //create your first component
 const Layout = () => {
@@ -19,24 +13,15 @@ const Layout = () => {
 	return (
 		<div className="d-flex flex-column">
 			<BrowserRouter basename={basename}>
-				<ScrollToTop>
-					<Navbar />
-					<Switch>
-						<Route exact path="/">
-							<Home />
-						</Route>
-						<Route exact path="/demo">
-							<Demo />
-						</Route>
-						<Route exact path="/single/:theid">
-							<Single />
-						</Route>
-						<Route>
-							<h1>Not found!</h1>
-						</Route>
-					</Switch>
-					<Footer />
-				</ScrollToTop>
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+
+					<Route>
+						<h1>Not found!</h1>
+					</Route>
+				</Switch>
 			</BrowserRouter>
 		</div>
 	);
