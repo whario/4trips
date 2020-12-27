09677966453bd755872c46671e1c757dbc98d185
Dariwoos <tst.info20@gmail.com>
2020-12-27 12:20:19 +0000

    Navbar

--- src/js/layout.js
+++ src/js/layout.js
@@ -17,8 +17,6 @@ const Layout = () => {
 					<Route exact path="/">
 						<Home />
 					</Route>
-					<Route exact path="/signUp" component={SignUp} />
-
 					<Route>
 						<h1>Not found!</h1>
 					</Route>
