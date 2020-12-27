f340689810943a06aef9607e28caddab8cf8a38c
Dariwoos <tst.info20@gmail.com>
2020-12-27 12:44:54 +0000

    arreglando develop

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "../../styles/home.scss";
-import LogIn from "../Component/LogIn.js";
-import Navbar from "../Component/Navbar.js";
+
 export const Home = () => {
-	return <Navbar />;
+	return <div></div>;
 };
