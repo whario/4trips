9d058cb8e059879998b84781aeea19d2232b68c7
Dariwoos <tst.info20@gmail.com>
2020-12-30 19:07:45 +0000

    corrigiendo logica

--- src/js/views/SignUp.js
+++ src/js/views/SignUp.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "../../styles/SignUp.css";
+import bootstrap from "react-bootstrap";
 ///Componentes
-import Navbar from "../Component/Navbar.js";
-import Footer from "../Component/Footer.js";
 
 const SignUp = () => {
 	const [datos, setDatos] = useState({
@@ -11,29 +10,36 @@ const SignUp = () => {
 		password: "",
 		repeatPassword: ""
 	});
+
 	const [submited, setSubmited] = useState(false);
+
 	const [valied, setValied] = useState(false);
+
 	const handelChange = e => {
-		setDatos({ ...datos, [e.targer.name]: e.targer.value });
+		setDatos({ ...datos, [e.target.name]: e.target.value });
 	};
 
 	const handleSubmit = event => {
 		event.preventDefault();
-		if (datos.userName && datos.email && datos.password && datos.repeatPassword) {
+		if (datos.userName && datos.email && datos.password == datos.repeatPassword) {
 			setValied(true);
 		}
 		setSubmited(true);
 	};
 
 	return (
-		<div>
-			<Navbar />
+		<div className="cont">
 			<form
-				className="myForm m-5 col-xs-12 col-md "
+				className="myForm m-5 col-sm-12 offset-md-3 col-md-6 offset-lg-3 col-lg-6 offset-lg-3 offset-xl-3 col-xl-6 offset-xl-3"
 				onChange={handelChange}
 				onSubmit={handleSubmit}>
-				<div className="form-row ">
-					{submited && valied ? <h3>Done!</h3> : null}
+				<div className="form    ">
+					{submited && valied && datos.password == datos.repeatPassword ? (
+						<div className="alert alert-success" role="alert">
+							This is a success alert—check it out!
+						</div>
+					) : null}
+					<br />
 					<div className="col-md-6 mb-3">
 						<label value="validationServer01">Nomber de usuario</label>
 						<input
@@ -44,6 +50,7 @@ const SignUp = () => {
 							id="validationServer01"
 						/>
 						{submited && !datos.userName ? <span>Escribe su nombre de usuario por favor</span> : null}
+						<br />
 						<label value="validationServer01">Correo electronico</label>
 						<input
 							name="email"
@@ -53,6 +60,7 @@ const SignUp = () => {
 							value={datos.email}
 						/>
 						{submited && !datos.email ? <span>Escribe su correo electronico por favor</span> : null}
+						<br />
 						<label value="validationServer01">Password</label>
 						<input
 							name="password"
@@ -62,6 +70,7 @@ const SignUp = () => {
 							value={datos.password}
 						/>
 						{submited && !datos.password ? <span>Escribe su contraseña por favor</span> : null}
+						<br />
 						<label value="validationServer01">Confirmacion de contraseña</label>
 						<input
 							name="repeatPassword"
@@ -73,13 +82,13 @@ const SignUp = () => {
 						{submited && datos.repeatPassword != datos.password ? (
 							<span>Confirma su contraseña bien porfavor </span>
 						) : null}
+						<br />
 					</div>
 				</div>
 				<button className="btn btn-primary" type="submit" value="sign up">
 					Register
 				</button>
 			</form>
-			<Footer />
 		</div>
 	);
 };
