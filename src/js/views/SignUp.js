53423d6b0ce095ac0ed9e353167309530e90d27e
Dariwoos <tst.info20@gmail.com>
2020-12-31 14:34:49 +0000

    terminado registro de usuario viajero

--- src/js/views/SignUp.js
+++ src/js/views/SignUp.js
@@ -28,67 +28,74 @@ const SignUp = () => {
 	};
 
 	return (
-		<div className="cont">
-			<form
-				className="myForm m-5 col-sm-12 offset-md-3 col-md-6 offset-lg-3 col-lg-6 offset-lg-3 offset-xl-3 col-xl-6 offset-xl-3"
-				onChange={handelChange}
-				onSubmit={handleSubmit}>
-				<div className="form    ">
-					{submited && valied && datos.password == datos.repeatPassword ? (
-						<div className="alert alert-success" role="alert">
-							This is a success alert—check it out!
-						</div>
-					) : null}
-					<br />
-					<div className="col-md-6 mb-3">
-						<label value="validationServer01">Nomber de usuario</label>
-						<input
-							name="userName"
-							value={datos.userName}
-							type="text"
-							className="form-control"
-							id="validationServer01"
-						/>
-						{submited && !datos.userName ? <span>Escribe su nombre de usuario por favor</span> : null}
-						<br />
-						<label value="validationServer01">Correo electronico</label>
-						<input
-							name="email"
-							placeholder="email"
-							type="email"
-							className="form-control"
-							value={datos.email}
-						/>
-						{submited && !datos.email ? <span>Escribe su correo electronico por favor</span> : null}
-						<br />
-						<label value="validationServer01">Password</label>
-						<input
-							name="password"
-							placeholder="Password"
-							type="password"
-							className="form-control"
-							value={datos.password}
-						/>
-						{submited && !datos.password ? <span>Escribe su contraseña por favor</span> : null}
-						<br />
-						<label value="validationServer01">Confirmacion de contraseña</label>
-						<input
-							name="repeatPassword"
-							placeholder="Confirm password"
-							type="password"
-							className="form-control"
-							value={datos.repeatPassword}
-						/>
-						{submited && datos.repeatPassword != datos.password ? (
-							<span>Confirma su contraseña bien porfavor </span>
+		<div className="container-fluid">
+			<div className="row justify-content-center">
+				<div className="offset-sm-2 col-sm-8 offset-sm-2  col-sm-6 col-md-3">
+					<form
+						className="form-container myForm justify-content-center"
+						onChange={handelChange}
+						onSubmit={handleSubmit}>
+						{submited && valied && datos.password == datos.repeatPassword ? (
+							<div className="alert alert-success" role="alert">
+								<p className="P">Registrado con éxito</p>
+							</div>
 						) : null}
 						<br />
-					</div>
+						<div className="ml-2 form-group">
+							<label value="validationServer01">Nomber de usuario</label>
+							<input
+								name="userName"
+								placeholder="Usuario"
+								value={datos.userName}
+								type="text"
+								className="form-control"
+								id="validationServer01"
+							/>
+							{submited && !datos.userName ? <span>Escribe su nombre de usuario por favor</span> : null}
+							<br />
+
+							<label value="validationServer01">Correo electronico</label>
+							<input
+								name="email"
+								placeholder="Correo electronico"
+								type="email"
+								className="form-control"
+								value={datos.email}
+							/>
+							{submited && !datos.email ? <span>Escribe su correo electronico por favor</span> : null}
+							<br />
+							<label value="validationServer01">Contraseña</label>
+
+							<input
+								name="password"
+								placeholder="Contraseña"
+								type="password"
+								className="form-control"
+								value={datos.password}
+							/>
+							{submited && !datos.password ? <span>Escribe su contraseña por favor</span> : null}
+							<br />
+							<label value="validationServer01">Confirmacion de contraseña</label>
+
+							<input
+								name="repeatPassword"
+								placeholder="Repite contraseña"
+								type="password"
+								className="form-control"
+								value={datos.repeatPassword}
+							/>
+							{submited && datos.repeatPassword != datos.password ? (
+								<span>Confirma su contraseña bien porfavor </span>
+							) : null}
+							<br />
+
+							<button className="btn btn-primary btn-block" type="submit" value="sign up">
+								Register
+							</button>
+						</div>
+					</form>
 				</div>
-				<button className="btn btn-primary" type="submit" value="sign up">
-					Register
-				</button>
-			</form>
+			</div>
 		</div>
 	);
 };
