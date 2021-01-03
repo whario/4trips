defe798929fe41e758da00c75eb6d5919f00d60d
Dariwoos <tst.info20@gmail.com>
2021-01-03 15:20:49 +0000

    adding style

--- src/js/views/LogIn.js
+++ src/js/views/LogIn.js
@@ -3,18 +3,21 @@ import "../../styles/Login.css";
 import "bootstrap/dist/css/bootstrap.css";
 
 export default function LogIn() {
-	const onSubmit = data => console.log(data);
-
 	return (
 		<div className="container">
 			<div className="navbar" />
-			<div className="row">
+			<div className="row justify-content-center">
 				<div className="col-12 col-md-6 ">
 					<form className="myFormLogIn">
-						<input className="form-control" type="email" placeholder="Correo electronico" />
+						<h3 className="h3"> Inicia sesion</h3>
+						<label className="label1"> Correo electronico</label>
+						<input className="form-control mt-5 " type="email" placeholder="Correo electronico" />
+						<br />
+						<label className="label2">Contraseña</label>
 						<input className="form-control" type="password" placeholder="Contraseña" />
-						<button className="btn btn-primary" type="submit">
-							Iniciar sesión
+						<br />
+						<button type="button" className="btn btn-primary">
+							Iniciar sesion
 						</button>
 					</form>
 				</div>
