eaa48a130fc63f366d1eedd985c821f08d4fdc62
Dariwoos <tst.info20@gmail.com>
2021-01-04 11:18:30 +0000

    añadir footer

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,5 +1,11 @@
 import React from "react";
-
+import Footer from "../Component/Footer.jsx";
+import Navbar from "../Component/Navbar.js";
 export const Home = () => {
-	return <div />;
+	return (
+		<div>
+			<Navbar />
+			<Footer />
+		</div>
+	);
 };
