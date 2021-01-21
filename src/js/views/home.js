2e97754920537667cf0c4ce548491c2115655119
whario <mariocepedaortega@gmail.com>
2021-01-21 20:35:26 +0000

    dejando develop a falta de cards

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Footer from "../Component/Footer.jsx";
 import Navbar from "../Component/Navbar.js";
+
 import SignUp from "./SignUp.js";
+
 export const Home = () => {
 	return (
 		<div>
 			<Navbar />
-			<SignUp />
 			<Footer />
 		</div>
 	);
