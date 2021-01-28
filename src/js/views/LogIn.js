a1c7b873ee617b4f18b32c2c24d6b4ce45b6f849
Dariwoos <tst.info20@gmail.com>
2021-01-28 20:09:58 +0000

    saliendo a registro'

--- src/js/views/LogIn.js
+++ src/js/views/LogIn.js
@@ -4,6 +4,13 @@ import logo4Trips from "../../img/logo_4Trips.png";
 import "bootstrap/dist/css/bootstrap.css";
 
 export default function LogIn() {
+        const [state, setState] = useState({
+            email:"",
+            password:""
+        });
+
+        const [validate, setValidate] = useState(false);
+        
 	return (
 		<div className="container">
 			<div className="navbar" />
@@ -13,10 +20,11 @@ export default function LogIn() {
 						<img className="logo4Trip" src={logo4Trips} />
 						<h3 className="h3"> Inicia sesion</h3>
 						<label className="label1"> Correo electronico</label>
-						<input className="form-control  " type="email" placeholder="Correo electronico" />
+						<input className="form-control  " type="email" placeholder="Correo electronico" name='email'/>
+                        {}
 						<br />
 						<label className="label2">Contraseña</label>
-						<input className="form-control" type="password" placeholder="Contraseña" />
+						<input className="form-control" type="password" placeholder="Contraseña" name='password'/>
 						<br />
 						<button type="button" className="btn btn-primary btn-block">
 							Iniciar sesion
