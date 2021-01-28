b139a506afb6f0beb4c484461de91a863d2dd657
Dariwoos <tst.info20@gmail.com>
2021-01-28 20:23:27 +0000

    haciendo logica de msg de error

--- src/js/views/LogIn.js
+++ src/js/views/LogIn.js
@@ -4,27 +4,34 @@ import logo4Trips from "../../img/logo_4Trips.png";
 import "bootstrap/dist/css/bootstrap.css";
 
 export default function LogIn() {
-        const [state, setState] = useState({
-            email:"",
-            password:""
-        });
+	const [state, setState] = useState({
+		email: "",
+		password: ""
+	});
 
-        const [validate, setValidate] = useState(false);
-        
+	const [validate, setValidate] = useState(false);
+
+	const handleSubmit = event => {
+		event.preventDefault();
+		if (state.email && state.password != "") {
+			setValidate(true);
+		}
+	};
+
 	return (
 		<div className="container">
 			<div className="navbar" />
 			<div className="row justify-content-center">
 				<div className="col-12 col-md-6 ">
-					<form className="myFormLogIn justify-content-center">
+					<form className="myFormLogIn justify-content-center" onSubmit={handleSubmit}>
 						<img className="logo4Trip" src={logo4Trips} />
 						<h3 className="h3"> Inicia sesion</h3>
 						<label className="label1"> Correo electronico</label>
-						<input className="form-control  " type="email" placeholder="Correo electronico" name='email'/>
-                        {}
+						<input className="form-control  " type="email" placeholder="Correo electronico" name="email" />
+						{sstate.email == "" && validate == false ? <span>ROJO</span> : null}
 						<br />
 						<label className="label2">Contraseña</label>
-						<input className="form-control" type="password" placeholder="Contraseña" name='password'/>
+						<input className="form-control" type="password" placeholder="Contraseña" name="password" />
 						<br />
 						<button type="button" className="btn btn-primary btn-block">
 							Iniciar sesion
