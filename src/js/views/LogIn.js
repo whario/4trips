18cdb6ca556c0df7e4a910afffc40d99355c2a01
whario <mariocepedaortega@gmail.com>
2021-02-07 19:13:42 +0000

    creando enlace a registro

--- src/js/views/LogIn.js
+++ src/js/views/LogIn.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "../../styles/Login.css";
 import logo4Trips from "../../img/logo_4Trips.png";
 import "bootstrap/dist/css/bootstrap.css";
+import { Link } from "react-router-dom";
 
 export default function LogIn() {
 	const [state, setState] = useState({
@@ -29,7 +30,7 @@ export default function LogIn() {
 		event.preventDefault();
 		console.log(state.email, "state.email");
 		if (state.email == "" && state.password == "") {
-			setError({ ...error, email: "por favor agrega su correo", password: "agrega su contraseña " });
+			setError({ ...error, email: "Introduce tu email", password: "Introduce tu contraseña" });
 		}
 	};
 
@@ -43,14 +44,17 @@ export default function LogIn() {
 						onChange={handleChange}>
 						<img className="logo4Trip" src={logo4Trips} />
 						<h3 className="h3"> Inicia sesion</h3>
-						<label className="label1"> Correo electronico</label>
-						<input className="form-control  " type="email" placeholder="Correo electronico" name="email" />
+						<label className="label1"> Email</label>
+						<input className="form-control  " type="email" placeholder="Email" name="email" />
 						{error.email != "" ? <span className="msg-error-login"> {error.email} </span> : null}
 						<br />
 						<label className="label2">Contraseña</label>
 						<input className="form-control" type="password" placeholder="Contraseña" name="password" />
 						{error.password != "" ? <span className="msg-error-login"> {error.password} </span> : null}
 						<br />
+						<p>
+							No tienes cuenta. Crea gratis una <Link to="/elige/tipo/deusuario">aquí</Link>
+						</p>
 						<button type="submit" className="btn btn-primary btn-block btn-login">
 							Iniciar sesion
 						</button>
